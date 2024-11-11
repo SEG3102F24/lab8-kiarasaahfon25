7190e7dae395ab527a3f54490fc22612ec40dae3
Kiara Saahfon <114544229+kiarasaahfon25@users.noreply.github.com>
2024-11-10 19:48:31 -0500

    Add files via upload

--- src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
+++ src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
@@ -1,8 +1,38 @@
 package seg3x02.employeeGql.resolvers
 
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
+import seg3x02.employeeGql.entity.Employee
+import seg3x02.employeeGql.repository.EmployeesRepository
+import seg3x02.employeeGql.resolvers.types.CreateEmployeeInput
 
 @Controller
-class EmployeesResolver {
+class EmployeesResolver(private val employeeRepository: EmployeesRepository) {
 
+    @QueryMapping
+    fun employees(): List<Employee> = employeeRepository.findAll()
+
+    @QueryMapping
+    fun employeeById(@Argument id: String): Employee? = employeeRepository.findById(id).orElse(null)
+
+    @MutationMapping
+    fun newEmployee(@Argument createEmployeeInput: CreateEmployeeInput): Employee {
+        val employee = Employee(
+            name = createEmployeeInput.name,
+            dateOfBirth = createEmployeeInput.dateOfBirth,
+            city = createEmployeeInput.city,
+            salary = createEmployeeInput.salary,
+            gender = createEmployeeInput.gender,
+            email = createEmployeeInput.email
+        )
+        return employeeRepository.save(employee)
+    }
+
+    @MutationMapping
+    fun deleteEmployee(@Argument id: String): Boolean {
+        employeeRepository.deleteById(id)
+        return true
+    }
 }
